354d389cedba1c6e1ba6ad1fc7a3c08fbc71aea3
Miles Blake <187061026+miles-blake@users.noreply.github.com>
2025-02-26 14:11:44 -0700

    Initial commit of MVC template

---
+++ Mission08_Team0311/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission08_Team0311.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
